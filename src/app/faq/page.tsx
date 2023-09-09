ba944614d25579cf613519bd6e0adb2f8cf655f3
Brent Mitton <brent.mitton+github@gmail.com>
2023-09-09 16:14:47 -0400

    flesh out faq a bit more

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+import { useState } from "react";
+import { UpChevron } from "./upchevron";
+import { DownChevron } from "./downchevron";
+
 export default function FrequentlyAskedQuestions() {
+  const [showQ1, setShowQ1] = useState(false);
+
   return (
     <section className="py-10 bg-gray-50 sm:py-16 lg:py-24">
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
@@ -6,175 +13,36 @@ export default function FrequentlyAskedQuestions() {
           <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
             Frequently Asked Questions
           </h2>
-          <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-          </p>
+          <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600"></p>
         </div>
 
         <div className="max-w-3xl mx-auto mt-8 space-y-4 md:mt-16">
           <div className="transition-all duration-200 bg-white border border-gray-200 shadow-lg cursor-pointer hover:bg-gray-50">
             <button
               type="button"
+              onClick={() => setShowQ1(!showQ1)}
               className="flex items-center justify-between w-full px-4 py-5 sm:p-6"
             >
               <span className="flex text-lg font-semibold text-black">
                 {" "}
-                How to create an account?{" "}
+                Where is the wedding taking place?{" "}
               </span>
 
-              <svg
-                className="w-6 h-6 text-gray-400 rotate-180"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
+              {showQ1 && <UpChevron />}
+              {!showQ1 && <DownChevron />}
             </button>
 
-            <div className="px-4 pb-5 sm:px-6 sm:pb-6">
-              <p>
-                Amet minim mollit non deserunt ullamco est sit{" "}
-                <a
-                  href="#"
-                  title=""
-                  className="text-blue-600 transition-all duration-200 hover:underline"
-                >
-                  aliqua dolor
-                </a>{" "}
-                do amet sint. Velit officia consequat duis enim velit mollit.
-              </p>
-            </div>
-          </div>
-
-          <div className="transition-all duration-200 bg-white border border-gray-200 cursor-pointer hover:bg-gray-50">
-            <button
-              type="button"
-              className="flex items-center justify-between w-full px-4 py-5 sm:p-6"
+            <div
+              className={`px-4 pb-5 sm:px-6 sm:pb-6 ${showQ1 ? "" : "hidden"}`}
             >
-              <span className="flex text-lg font-semibold text-black">
-                {" "}
-                How can I make payment using Paypal?{" "}
-              </span>
-
-              <svg
-                className="w-6 h-6 text-gray-400"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-
-            <div className="hidden px-4 pb-5 sm:px-6 sm:pb-6">
               <p>
-                Amet minim mollit non deserunt ullamco est sit{" "}
-                <a
-                  href="#"
-                  title=""
-                  className="text-blue-600 transition-all duration-200 hover:underline"
-                >
-                  aliqua dolor
-                </a>{" "}
-                do amet sint. Velit officia consequat duis enim velit mollit.
+                The ceremony and dinner reception will be held at Fox Meadow
+                Golf Course the afternoon of July 20th, 2024 (Time TBA). A
+                cocktail hour with hor d'oeuvres will take place between the
+                ceremony and reception.
               </p>
-            </div>
-          </div>
-
-          <div className="transition-all duration-200 bg-white border border-gray-200 cursor-pointer hover:bg-gray-50">
-            <div className="">
-              <button
-                type="button"
-                className="flex items-center justify-between w-full px-4 py-5 sm:p-6"
-              >
-                <span className="flex text-lg font-semibold text-black">
-                  {" "}
-                  Can I cancel my plan?{" "}
-                </span>
-
-                <svg
-                  className="w-6 h-6 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </button>
-
-              <div className="hidden px-4 pb-5 sm:px-6 sm:pb-6">
-                <p>
-                  Amet minim mollit non deserunt ullamco est sit{" "}
-                  <a
-                    href="#"
-                    title=""
-                    className="text-blue-600 transition-all duration-200 hover:underline"
-                  >
-                    aliqua dolor
-                  </a>{" "}
-                  do amet sint. Velit officia consequat duis enim velit mollit.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="transition-all duration-200 bg-white border border-gray-200 cursor-pointer hover:bg-gray-50">
-            <button
-              type="button"
-              className="flex items-center justify-between w-full px-4 py-5 sm:p-6"
-            >
-              <span className="flex text-lg font-semibold text-black">
-                {" "}
-                How can I reach to support?{" "}
-              </span>
-
-              <svg
-                className="w-6 h-6 text-gray-400"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-
-            <div className="hidden px-4 pb-5 sm:px-6 sm:pb-6">
-              <p>
-                Amet minim mollit non deserunt ullamco est sit{" "}
-                <a
-                  href="#"
-                  title=""
-                  className="text-blue-600 transition-all duration-200 hover:underline"
-                >
-                  aliqua dolor
-                </a>{" "}
-                do amet sint. Velit officia consequat duis enim velit mollit.
-              </p>
+              <br />
+              <p>Partying to follow. 🍻🍷💃</p>
             </div>
           </div>
         </div>
@@ -182,11 +50,11 @@ export default function FrequentlyAskedQuestions() {
         <p className="text-center text-gray-600 textbase mt-9">
           Didn’t find the answer you are looking for?{" "}
           <a
-            href="#"
+            href="mailto:wedding@jessandbrent.ca"
             title=""
             className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
           >
-            Contact our support
+            Email us!
           </a>
         </p>
       </div>
---
+++ src/app/faq/upchevron.tsx
@@ -0,0 +1,18 @@
+export const UpChevron = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-gray-400 rotate-180"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M19 9l-7 7-7-7"
+      />
+    </svg>
+  );
+};
