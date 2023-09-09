7ae7b94a2cd7935a6ce79f9028f1d404a49e80b3
Brent Mitton <brent.mitton+github@gmail.com>
2023-09-09 16:45:42 -0400

    add the divider image

--- src/app/divider.tsx
+++ src/app/divider.tsx
@@ -1,8 +1,19 @@
+"use client";
+import Image from "next/image";
 export const Divider = () => {
   return (
-    <div className="flex items-center justify-center px-6 py-12 mx-auto">
-      Hi BB, can you find a cute little divider image to go here to separate the
-      sections. Maybe some little floral splash or something
+    <div className="flex items-center justify-center px-6 py-12 mx-auto mt-2">
+      <Image
+        alt="Pretty flowers"
+        className="h-auto "
+        style={{ transform: "translate3d(0, 0, 0)" }}
+        src={
+          "https://res.cloudinary.com/dii1nnhpp/image/upload/v1694292052/assets/floraldivider_q6lfol.png"
+        }
+        width={978 / 2}
+        height={248}
+        loading="lazy"
+      />
     </div>
   );
 };
