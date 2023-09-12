c3b4803e7db2b074c51cacad880b6274360f6056
Brent Mitton <brent.mitton+github@gmail.com>
2023-09-12 17:43:19 -0400

    Revert "remove everything"
    This reverts commit 5dd8ecee94e17881f9ca46a0bb1c5144138b6684.

--- src/app/divider.tsx
+++ src/app/divider.tsx
@@ -2,10 +2,10 @@
 import Image from "next/image";
 export const Divider = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center px-6 py-12 mx-auto mt-2">
       <Image
         alt="Pretty flowers"
-        className=""
+        className="h-auto "
         style={{ transform: "translate3d(0, 0, 0)" }}
         src={
           "https://res.cloudinary.com/dii1nnhpp/image/upload/v1694292052/assets/floraldivider_q6lfol.png"
