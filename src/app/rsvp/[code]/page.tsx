8f609ade89d139bf9d4e87edbbdd388eba4f36d9
Brent Mitton <brent.mitton@gmail.com>
2024-03-03 19:31:02 -0500

    string interpolation

--- src/app/rsvp/[code]/page.tsx
+++ src/app/rsvp/[code]/page.tsx
@@ -94,10 +94,10 @@ export default function RSVPCode({ params }: { params: { code: string } }) {
         <div className="max-w-2xl mx-auto text-center">
           <h3 className="text-xl font-bold leading-tight text-black sm:text-xl lg:text-xl">
             {initialValues.numGuests === 2
-              ? "RSVP For {initialValues.rsvpName} and Guest"
+              ? `RSVP For ${initialValues.rsvpName} and Guest`
               : params.code === "BYJL" || params.code === "WWCJ"
-              ? "RSVP For {initialValues.rsvpName} and Guest"
-              : "RSVP For {initialValues.rsvpName}"}
+              ? `RSVP For ${initialValues.rsvpName} and Guest`
+              : `RSVP For ${initialValues.rsvpName}`
           </h3>
           <div>
             You can come back to this page at any time to change any details if
